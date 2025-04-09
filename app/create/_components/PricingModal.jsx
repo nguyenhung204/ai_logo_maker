cc3068914956defb2fb2682af1585f9594d516cf
nguyenhung <nguyenhung2004200@gmial.com>
2025-04-10 00:53:24 +0700

    Create and Render(UI) : Create UI Pricing and Payment after input session, step 6

--- app/create/_components/PricingModal.jsx
+++ app/create/_components/PricingModal.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import HeadingDescription from './HeadingDescription'
+import Lookup from '@/app/_data/Lookup'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
 function PricingModal() {
   return (
     <div className='my-10'>
-    
+        <HeadingDescription
+         title={Lookup.LogoPricingModelTitle}
+         description={Lookup.LogoPricingModelDesc}/>
+
+         <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-10'>
+            {Lookup.pricingOption.map((pricing, index) => (
+                <div key={index} className='flex flex-col items-center p-5 border rounded-xl'>
+                    <Image src={pricing?.icon} alt={pricing?.title}
+                    width={60} height={60}/>
+                    <h2 className='font-medium text-2xl'>{pricing.title}</h2>
+                    <div>
+                        {pricing.features.map((feature, index) => (
+                            <h2 className='text-lg mt-3' key={index}>{feature}</h2>
+                        ))}
+                        <Button className= 'mt-5'>{pricing.button}</Button>
+                    </div>
+                </div>
+            ))}
+         </div>
+
     </div>
   )
 }
