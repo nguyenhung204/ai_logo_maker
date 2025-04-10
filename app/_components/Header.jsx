dc92b973da64ca7cd71603bb4a728a3f2a22f684
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-04-10 19:20:06 +0700

    Implement authentication, add sign-in/sign-out buttons to header, fix logo title not included in form data

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,15 +1,41 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Header = () => {
+  const { user } = useUser();
+
   return (
     <div className="px-10 lg:px-32 xl:px-48 2xl:px-56 p-4 flex justify-between items-center shadow-sm">
       <Link href={"/"}>
         <Image src={"logo.svg"} width={180} height={180} alt="Logo" />
       </Link>
-      <Button>Get Started</Button>
+      <div className="flex gap-3 items-center">
+        <Button className="min-w-[100px]">Get Started</Button>
+        {!user && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="signin" className="min-w-[100px]">
+                Sign In
+              </Button>
+            </SignInButton>
+            <SignUpButton>Sign Up</SignUpButton>
+          </>
+        )}
+
+        {user && (
+          <>
+            <Button variant="outline" mode="modal">
+              Dashboard
+            </Button>
+            <UserButton />
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- app/create/_components/LogoTitle.jsx
+++ app/create/_components/LogoTitle.jsx
@@ -1,23 +1,33 @@
-'use client'
-import React, { useState } from 'react'
-import HeadingDescription from './HeadingDescription'
-import Lookup from '@/app/_data/Lookup'
-import { useSearchParams } from 'next/navigation'
+"use client";
+import React, { useEffect, useState } from "react";
+import HeadingDescription from "./HeadingDescription";
+import Lookup from "@/app/_data/Lookup";
+import { useSearchParams } from "next/navigation";
 
-function LogoTitle({onHandleInputChange}) {
-    const searchParams = useSearchParams();
-    const [title, setTitle] = useState(searchParams.get('title') ?? '');
-    return (
-        <div className='my-10'>
-            <HeadingDescription 
-            title={Lookup.LogoTitle} 
-            description={Lookup.LogoTitleDesc} />
-            <input type="text" placeholder={Lookup.InputTitlePlaceholder} 
-            className='p-4 border rounded-lg mt-5 w-full'
-            value={title}
-            onChange={(e) => onHandleInputChange(e.target.value)} />
-        </div>
-    )
+function LogoTitle({ onHandleInputChange, formData }) {
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    const initialTitle = searchParams.get("title") ?? "";
+    if (initialTitle) {
+      onHandleInputChange(initialTitle);
+    }
+  }, []);
+
+  return (
+    <div className="my-10">
+      <HeadingDescription
+        title={Lookup.LogoTitle}
+        description={Lookup.LogoTitleDesc}
+      />
+      <input
+        type="text"
+        placeholder={Lookup.InputTitlePlaceholder}
+        className="p-4 border rounded-lg mt-5 w-full"
+        value={formData?.title ?? ""}
+        onChange={(e) => onHandleInputChange(e.target.value)}
+      />
+    </div>
+  );
 }
 
-export default LogoTitle
+export default LogoTitle;
