7d5759f33dc18f89f9e7335c79cbf0f7bc8324fa
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-04-18 14:30:25 +0700

    minor ajust

--- config/FirebaseConfig.jsx
+++ config/FirebaseConfig.jsx
@@ -3,13 +3,23 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: "ai-logo-maker-4ea1f.firebaseapp.com",
+//   projectId: "ai-logo-maker-4ea1f",
+//   storageBucket: "ai-logo-maker-4ea1f.firebasestorage.app",
+//   messagingSenderId: "262611841435",
+//   appId: "1:262611841435:web:57e73fcd8a5c8a0b9c313e"
+// };
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "ai-logo-maker-4ea1f.firebaseapp.com",
-  projectId: "ai-logo-maker-4ea1f",
-  storageBucket: "ai-logo-maker-4ea1f.firebasestorage.app",
-  messagingSenderId: "262611841435",
-  appId: "1:262611841435:web:57e73fcd8a5c8a0b9c313e"
+  authDomain: "fir-playground-fb53c.firebaseapp.com",
+  projectId: "fir-playground-fb53c",
+  storageBucket: "fir-playground-fb53c.firebasestorage.app",
+  messagingSenderId: "958398128009",
+  appId: "1:958398128009:web:bdd9eaafce9fe479a74b58",
+  measurementId: "G-135KK9J31Q",
 };
 
 // Initialize Firebase
